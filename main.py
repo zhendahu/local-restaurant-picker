5db69a4ccc33c8c14409face507338aeab1d5cc9
zhendahu <zhendahu@gmail.com>
2022-11-16 21:34:49 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import requests
+import _json
+
+yelp_api_key = 'WGHchmkDPyjJchvhWJ7IjTZaY5GqlGwN7pO4RJHgG_uV12Ch_kVRqYH1-l7Yv1ln7BOAjakR2N2S1h94bI1662IZNvcy70IRMymUIu2SwaRPs-MIMfe-iTgLRsF1Y3Yx'
+headers = {'Authorization': 'Bearer %s' % yelp_api_key}
